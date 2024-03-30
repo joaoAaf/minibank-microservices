7b35d5553352f59c14d791eb86f5d4b6990d763d
joaoAaf <joao.anderson.af@gmail.com>
2024-03-30 09:10:46 -0300

    criando entidade Customer

--- account-service/src/main/java/estudo/accountservice/model/Customer.java
+++ account-service/src/main/java/estudo/accountservice/model/Customer.java
@@ -1,5 +1,26 @@
 package estudo.accountservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "client")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account",nullable = false, unique = true)
+    private String accountNum;
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+    @Column(name = "name",nullable = false, length = 100)
+    private String fullName;
+    @Column(nullable = false)
+    private boolean enable;
 
 }
