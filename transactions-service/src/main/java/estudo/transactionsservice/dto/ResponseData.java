26ee8bfe1539ce0120509a2f3f1a1c2badb7e66d
João Anderson <joao.anderson.af@gmail.com>
2024-04-07 10:10:06 -0300

    transformando ResponseData em Record

--- transactions-service/src/main/java/estudo/transactionsservice/dto/ResponseData.java
+++ transactions-service/src/main/java/estudo/transactionsservice/dto/ResponseData.java
@@ -4,16 +4,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Builder;
-import lombok.Data;
 
 @Builder
-@Data
 // Se um atributo for nulo, o mesmo será excluido do Json
 @JsonInclude(value = Include.NON_NULL)
-public class ResponseData {
-
-        private Object data;
-        private String msg;
-        private String port;
+public record ResponseData(Object data, String msg, String port) {
 
 }
