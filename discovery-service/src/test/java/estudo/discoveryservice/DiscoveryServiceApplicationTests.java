ac0127c0fa44096838d2d3fdcf46c5c1fae9114f
joaoAaf <joao.anderson.af@gmail.com>
2024-04-26 16:43:17 -0300

    criando e configurando discovery-service (eureka client e service)

---
+++ discovery-service/src/test/java/estudo/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package estudo.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- transactions-service/src/main/java/estudo/transactionsservice/TransactionsServiceApplication.java
+++ transactions-service/src/main/java/estudo/transactionsservice/TransactionsServiceApplication.java
@@ -2,10 +2,12 @@ package estudo.transactionsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TransactionsServiceApplication {
 
 	public static void main(String[] args) {
